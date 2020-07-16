a37413f19df3f28e619cd5c29a68e69f6151e30d
ioilmio <illuminato.salvatore@gmail.com>
2020-07-16 19:59:08 +0200

    add user fields to devise model;use strong params for user;view for sign_up

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationController < Devise::RegistrationController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :spassword_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :spassword_confirmation, :current_password)
+  end
+end
